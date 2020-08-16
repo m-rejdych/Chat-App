b77d9223fb79a242a70bc31f3f9af9e9414f3f4d
Michał Rejdych <rejdych.m.r@gmail.com>
2020-08-16 23:21:13 +0200

    Initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnnXJImYm81dGQKnrjOotfwCo-3W-i3aA',
+  authDomain: 'chat-app-a6345.firebaseapp.com',
+  databaseURL: 'https://chat-app-a6345.firebaseio.com',
+  projectId: 'chat-app-a6345',
+  storageBucket: 'chat-app-a6345.appspot.com',
+  messagingSenderId: '314966886330',
+  appId: '1:314966886330:web:da326989e78447a27c3f83',
+};
+
+firebase.initializeApp(firebaseConfig);
